b419e59c5240f54c7a064f484eafb418fea2c911
Nikita Shalginskih <n.shalginskih@nc.rt.spb>
2022-06-22 17:26:58 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход
+// принимает число и выдаёт, является ли число чётным
+// (делится ли оно на два без остатка).
+{int a;
+    Console.WriteLine("Введите число : ");
+a = int.Parse(Console.ReadLine());;
+if(a % 2 == 0)
+{
+Console.WriteLine("Число четное ");
+}
+else
+{
+Console.WriteLine("Число нечетное ");
+}
+Console.ReadLine();} 
